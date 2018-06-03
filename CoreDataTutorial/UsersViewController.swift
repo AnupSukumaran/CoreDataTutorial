2695fb2b05b6c9bc9b2b38be83bea1a826aa19c2
Anup Sukumaran <anup.sukumaran9@gmail.com>
2018-06-03 07:00:46 +0530

    New

---
+++ CoreDataTutorial/UsersViewController.swift
@@ -0,0 +1,79 @@
+//
+//  UsersViewController.swift
+//  CoreDataTutorial
+//
+//  Created by Sukumar Anup Sukumaran on 02/06/18.
+//  Copyright © 2018 TechTonic. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class UsersViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var userArray:[User] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.fetchData()
+        self.tableView.reloadData()
+    }
+
+    func fetchData() {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+             userArray = try context.fetch(User.fetchRequest())
+        } catch let error {
+            print("COreDataError = \(error.localizedDescription)")
+        }
+        
+        
+    }
+    
+}
+
+extension UsersViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let name = userArray[indexPath.row]
+        cell.textLabel?.text = name.firstname! + " " + name.secondname!
+        
+        return cell
+    }
+    
+}
+
+extension UsersViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+        if editingStyle == .delete {
+            let user = userArray[indexPath.row]
+            context.delete(user)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            // fetch is used to update the array for the table
+            do{
+                userArray = try context.fetch(User.fetchRequest())
+            }catch let error {
+                print("CDE = \(error.localizedDescription)")
+            }
+        }
+        tableView.reloadData()
+    }
+    
+}
--- CoreDataTutorial/ViewController.swift
+++ CoreDataTutorial/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var secondName: UITextField!
+    
+    @IBOutlet weak var SearchTexField: UITextField!
+    
+    @IBOutlet weak var searchResultLabel: UILabel!
+    
+    // we are make a magic scatch pad for the data you input.
+    var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func saveUserButton(_ sender: Any) {
+        
+        if !(firstName.text?.isEmpty)! && !(secondName.text?.isEmpty)!{
+            
+            let newUser = NSEntityDescription.insertNewObject(forEntityName: "User", into: context)
+            newUser.setValue(self.firstName!.text, forKey: "firstname")
+            newUser.setValue(self.secondName!.text, forKey: "secondname")
+            
+            do{
+                try context.save()
+            }catch let error {
+                print("COREDATA ERROR = \(error.localizedDescription)")
+            }
+            
+        }else{
+            print("Please fill the textfields")
+        }
+        
     }
-
-
+    
+    @IBAction func searchButton(_ sender: Any) {
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        let searchString = self.SearchTexField.text
+        request.predicate = NSPredicate(format: "firstname = %@", searchString!)
+        
+        do {
+            let result:[NSManagedObject] = try context.fetch(request) as! [NSManagedObject]
+            if result.count > 0{
+                
+                let firstname = result[0].value(forKey: "firstname") as!String
+                let lastname = result[0].value(forKey: "secondname") as! String
+                
+                self.searchResultLabel?.text =  firstname + " " + lastname
+            }else {
+                
+                self.searchResultLabel?.text = "No User Found"
+                
+            }
+            
+//            for data in result as! [NSManagedObject] {
+//                fName = data.value(forKey: "firstname") as! String
+//                Lname = data.value(forKey: "secondname") as! String
+//            }
+//
+//            self.searchResultLabel.text =
+            
+        } catch let error  {
+            print("CoreData = \(error.localizedDescription)")
+        }
+        
+    }
+    
+    
 }
 
